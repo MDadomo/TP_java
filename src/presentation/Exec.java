c09209b8a11d27b465d629fa80bb5c62f2f715d3
DESKTOP-NBL3DAK\thoma <thomas.mahe@ynov.com>
2017-12-22 15:28:02 +0100

    no message

--- src/presentation/Exec.java
+++ src/presentation/Exec.java
@@ -6,13 +6,12 @@ import entities.Vainqueur;
 import entities.Villes;
 import metier.Combat;
 
-
 public class Exec {
 	public static void main(String[] args) throws SQLException {
-	Combat main = new Combat();
-	List l1 = main.creationVille();
-	List lville = main.SelectionJ1(l1);
-	String gagnantString = main.combatVilles(lville);
-	System.out.println("La ville qui gagne dans ce tournoi est : " + gagnantString + ".");
-}
+		Combat main = new Combat();
+		List l1 = main.creationVille();
+		List lville = main.SelectionJ1(l1);
+		String gagnantString = main.combatVilles(lville);
+		System.out.println("La ville qui gagne dans ce tournoi est : " + gagnantString + ".");
+	}
 }
