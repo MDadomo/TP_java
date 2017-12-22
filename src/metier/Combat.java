64df05633afe0e7e2aa42607a50a1d7dab078475
dadomo <dadomo.marie@gmail.com>
2017-12-22 09:46:20 +0100

    nom ville

--- src/metier/Combat.java
+++ src/metier/Combat.java
@@ -7,8 +7,14 @@ public class Combat {
 
     public List creationVille(){
 
-        Villes villeOne = new Villes(1, 2000, 403 , 44 ,"Nantes", 3);
-
+        Villes villeOne = new Villes(1, 2000, 4083 , 44 ,"Nantes", 3);
+        Villes villeDeux = new Villes(3, 40300, 3703 , 44 ,"St Sebastien sur loire", 2);
+        Villes villetTrois = new Villes(2, 4600, 3053 , 85 ,"Les Sables D'olonnes", 2);
+        Villes villeQuatre = new Villes(5, 47650, 303 , 66 ,"Nice", 2);
+        Villes villeCinq = new Villes(7, 408600, 3303 , 44 ,"St Herblain", 2);
+        Villes villeSix = new Villes(21, 434000, 3803 , 44 ,"St Bernad", 2);
+        Villes villeSept = new Villes(54, 40050, 3039 , 44 ,"St luce", 2);
+        Villes villeHuit = new Villes(6, 40020, 3039 , 44 ,"St Loire", 2);
         return null;
     }
 
