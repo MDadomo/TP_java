25993be5b7550dbc2e9e8bbce2516761e1bd0376
dadomo <dadomo.marie@gmail.com>
2017-12-22 09:23:28 +0100

    combat v1

--- src/metier/Combat.java
+++ src/metier/Combat.java
@@ -1,5 +1,15 @@
 package metier;
 
+import entities.Villes;
+
 public class Combat {
+
+
+    public List creationVille(){
+
+        Villes villeOne = Villes(1, 2000, 403 , "Loire Atlantique","Nantes", "premier");
+
+        return null;
+    }
 
 }
